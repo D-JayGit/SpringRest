bf3dcef68ced913c073407e166dc1284686e1477
Dhananjay <dsengraphwar@gmail.com>
2019-08-28 11:50:15 +0530

    First commit

---
+++ src/main/java/com/dj/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.dj.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.dj")
+public class AppConfig {
+
+}
